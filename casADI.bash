997e5fc9715589cfcc3e857311a606c25d306c59
mostmosthandsome <121703002+mostmosthandsome@users.noreply.github.com>
2024-07-06 16:51:56 +0800

    Update casADI.bash

--- casADI.bash
+++ casADI.bash
@@ -1,6 +1,5 @@
-cd ~
-mkdir software && cd software
-mkdir ipopt && cd ipopt
+cp ./coinhsl.zip ~/softwre/ipopt
+cd ~/software/ipopt
 # install ASL
 git clone https://github.com/coin-or-tools/ThirdParty-ASL.git
 cd ThirdParty-ASL
@@ -13,4 +12,33 @@ cd ..
 sudo apt install libblas-dev  liblapack-dev
 #install HSL
 git clone https://github.com/coin-or-tools/ThirdParty-HSL.git
+unzip coinhsl.zip -d ./ThirdParty-HSL
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+#install mumps
+git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
+cd ThirdParty-Mumps
+sudo ./get.Mumps
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+#install ipopt
+git clone https://github.com/coin-or/Ipopt.git
+cd Ipopt
+mkdir build && cd build
+../configure --with-mumps --with-mumps-cflags="-I/usr/local/include/coin-or/mumps" --with-mumps-lflags="-L/usr/local/lib -lcoinmumps"  --with-hsl-cflags="-I/usr/local/include/coin-or/hsl" --with-hsl-lflags="-L/usr/local/lib -lcoinhsl"
+sudo make
+sudo make install
 
+cd ..
+#install casADI
+git clone https://github.com/casadi/casadi.git
+cd casadi
+mkdir build
+cd build
+cmake -DWITH_PYTHON=ON -DWITH_PYTHON3=ON ..
+make
+sudo make install
